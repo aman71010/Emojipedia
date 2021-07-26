c88588b97b0016b660f24cd346e297e0a74447f8
Aman <amangupta71010@gmail.com>
2021-07-26 23:46:35 +0530

    Updates

---
+++ precache-manifest.b89daf52f64ddf70e38f2f81ac9ee3c3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467bc6d5e86ace70a287cf1343628faf",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "aee04822626c08a5b3a6",
+    "url": "/Emojipedia/static/js/2.564df045.chunk.js"
+  },
+  {
+    "revision": "8c91b4cebd1b575ec3c8",
+    "url": "/Emojipedia/static/js/main.0df489d2.chunk.js"
+  },
+  {
+    "revision": "6224630e21eed1f82ef3",
+    "url": "/Emojipedia/static/js/runtime-main.52cd7e16.js"
+  }
+]);
